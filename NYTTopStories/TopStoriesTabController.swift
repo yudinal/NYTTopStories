2efdcc469e12712e5d50135fe25c0afac1848991
Lilia Yudina <liliayudina@gmail.com>
2020-02-06 10:49:36 -0500

    Initial Commit

---
+++ NYTTopStories/TopStoriesTabController.swift
@@ -0,0 +1,21 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Lilia Yudina on 2/6/20.
+//  Copyright © 2020 Lilia Yudina. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+    
+
+
+}
