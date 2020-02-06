2df98102cddde6701cd3c02fbad70db89f4a901f
Lilia Yudina <liliayudina@gmail.com>
2020-02-06 11:39:59 -0500

    added collection view and search bar

--- NYTTopStories/NewsFeedViewController.swift
+++ NYTTopStories/NewsFeedViewController.swift
@@ -10,20 +10,44 @@ import UIKit
 
 class NewsFeedViewController: UIViewController {
 
+    private let newsFeedView = NewsFeedView()
+    
+    override func loadView() {
+        view = newsFeedView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground // white when dark mode is off, black when dark mode is on
+        
+        // setting up collection datasource and delegate
+        newsFeedView.collectionView.dataSource = self
+        newsFeedView.collectionView.delegate = self
+        
+        // register a collection view cell
+        newsFeedView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "acticleCell")
+    }
+
+}
+
+
+extension NewsFeedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 50
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "acticleCell", for: indexPath)
+        cell.backgroundColor = .white
+        return cell
     }
-    */
+    
+}
 
+extension NewsFeedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.30
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
 }
---
+++ NYTTopStories/Views/NewsFeedView.swift
@@ -0,0 +1,68 @@
+//
+//  NewsFeedView.swift
+//  NYTTopStories
+//
+//  Created by Lilia Yudina on 2/6/20.
+//  Copyright © 2020 Lilia Yudina. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedView: UIView {
+    
+    public lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.autocapitalizationType = .none
+        sb.placeholder = "search for article"
+        return sb
+    }()
+    
+    public lazy var collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: CGRect.zero,
+                                  collectionViewLayout: layout)
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupSearchBarConstraints()
+        setupCollectionViewConstraints()
+    }
+    
+    private func setupSearchBarConstraints() {
+        
+        addSubview(searchBar)
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
+        
+        ])
+    }
+    
+    private func setupCollectionViewConstraints() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
